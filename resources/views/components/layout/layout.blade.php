f3e04da35215af891d2499607d85a9663dddc624
Chris Coutts <ccoutts@mmgy.com>
2026-01-26 14:39:00 +0000

    Create flash message and toast functionality using alpine.js

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Idea</title>
-    @vite(['resources/css/app.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-background text-foreground">
     <x-layout.nav />
@@ -13,5 +13,8 @@
         {{ $slot }}
     </main>
 
+    @session('success')
+    <div x-data="{ show: true }" x-init="setTimeout(() => {show = false}, 3000)" x-show="show" x-transition.opacity.duration.300ms class="bg-primary px-4 py-3 absolute bottom-1 right-4 rounded-lg">{{ $value }}</div>
+    @endsession
 </body>
 </html>
